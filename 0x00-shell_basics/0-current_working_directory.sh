e3a2438d2669c5db7d23f86f46e9a71ca1754fed
Tawfiq Obeid <ttawfiq.obeid@gmail.com>
2023-10-03 15:38:54 -0700

    Update scripts for tasks 0 to 14

---
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script prints the absolute path name of the current working directory.
+
+# Use the 'pwd' command to get the absolute path of the current working directory
+current_directory=$(pwd)
+
+# Print the absolute path
+echo "$current_directory"
+
+
